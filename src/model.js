b6363ade66d7fa71a03a3d5cb87a0bae242807fa
samelie <sam@stinkdigital.com>
2017-06-13 01:14:29 -0700

    More sites

--- src/model.js
+++ src/model.js
@@ -1,8 +1,25 @@
 module.exports = (state, emitter) => {
+
+  emitter.on('site-clicked', (label) => {
+    console.log(label);
+    state.sites.forEach(site => {
+      site.selected = site.label === label
+    })
+  })
+
   state.sites = [{
     label: 'DeuxTube',
-    selected:true,
-    url: 'https://samelie.github.io/rad.-deuxtube/'
+    selected: true,
+    url: 'https://samelie.github.io/rad.wtf-deuxtube/'
+  },{
+    label: 'Alhambra',
+    selected: false,
+    url: 'https://samelie.github.io/thealhambraproject.com/desktop-tour/'
+  },{
+    label: 'Mars',
+    selected: false,
+    url: 'https://samelie.github.io/mars-amenothep/'
   }]
+
   emitter.emit('render')
 }
--- src/views/menu.js
+++ src/views/menu.js
@@ -1,14 +1,15 @@
 const html = require('choo/html')
 
-const generateEls = (sites) => (html `
+const generateEls = (sites, emit) => (html `
   <li
     class="sites--item"
+    onclick=${()=>(emit("site-clicked",sites.label))}
   >${sites.label}</li>`)
 
 module.exports = (sites, emit) => {
   return html `
         <ul>
-            ${generateEls(sites[0])}
+            ${generateEls(sites[0], emit)}
         </ul>
       `
 }
